439ed00f8080d16cf3de20bd19d31e09cd35871d
jfrasinska <j.frasinska@gmail.com>
2023-12-03 22:09:29 +0100

    crating contact app first step

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+
+const App = () => {
+  const [state, setState] = useState({
+    contacts: [],
+    name: '',
+  });
+
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    setState(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (state.name.trim() === '') {
+      alert('Please enter a valid name.');
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name: state.name.trim(),
+    };
+
+    setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+      name: '',
+    }));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Contact Book App</h1>
+      <ContactForm
+        name={state.name}
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit}
+      />
+      <ContactList contacts={state.contacts} />
     </div>
   );
 };
+
+export default App;
---
+++ src/components/ContactForm.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ContactForm = ({ name, handleInputChange, handleSubmit }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={handleInputChange}
+        />
+      </label>
+      <button type="submit">Add Contact</button>
+    </form>
+  );
+};
+
+export default ContactForm;
